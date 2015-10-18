f5393bd159145fb217160bb9cd62784f234dc4e9
vankasteelj <vankasteelj@gmail.com>
2015-10-18 16:30:16 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# detect-lang
+
+**Node.js module: input text, get language it's written in. It's that simple. **
+
+Based on Promises and thus working asynchronously (thanks to Bluebird), this module uses retext to analyse files & strings using Node.js
+
+------
+
+## Quick start
+
+    npm install detect-lang
+
+then:
+
+```js
+var detect = require('detect-lang');
+var input = "Quand tu sauras mon crime, et le sort qui m'accable, je n'en mourrai pas moins, j'en mourrai plus coupable." // Phèdre, Racine, I-3.
+
+detect(input)
+    .then(function(data) {
+        console.log(data);
+    });
+```
+
+Output:
+
+```js
+Object {
+    iso6391: 'fr',
+    iso6392: null,
+    terminologic: 'fra',
+    bibliographic: 'fre',
+    name: 'French',
+    probability: '100.00',
+    detected_langs: {
+        fra: 1
+    }
+}
+```
+
+*ISO-639 is a pretty common standard. However, for best results, the output of 'iso6392' can be `null` when there are terminologic and bibliographic variants. Read the explaination of the ISO-639-2 on Wikipedia if you're confused.*
+
+------
+
+## License
+
+This code is registered under GPLv3 - Copyright (c) 2015  Jean van Kasteel
+
+### The GNU GENERAL PUBLIC LICENSE (GPL)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/
---
+++ index.js
@@ -0,0 +1,108 @@
+var Promise = require('bluebird'),
+    fs = require('fs'),
+    path = require('path');
+
+var parseInput = function (input) {
+    return new Promise(function (resolve, reject) {
+        // test string
+        if (typeof input !== 'string' && !(input instanceof String)) {
+            throw new Error('Input must be a string');
+        }
+        // test file
+        if (fs.existsSync(path.normalize(input))) {
+            fs.readFile(path.normalize(input), function (err, data) {
+                if (err) throw err;
+                resolve(data.toString());
+            });
+        } else {
+            if (input.length < 64) throw new Error('Not enough text to process. At least 64 characters are expected.');
+            resolve(input);
+        }
+    });
+};
+
+var getLang = function (input) {
+    return new Promise(function (resolve, reject) {
+       require('retext').use(require('retext-language')).use(function () {
+           return function (cst) {
+               resolve(cst);
+           }
+       }).process(input)
+    });
+};
+
+var count = function(ary, classifier) {
+    return ary.reduce(function(counter, item) {
+        var p = (classifier || String)(item);
+        counter[p] = counter.hasOwnProperty(p) ? counter[p] + 1 : 1;
+        return counter;
+    }, {})
+};
+
+var parseLang = function (det) {
+    return new Promise(function (resolve, reject) {
+        var tmp = [];
+        
+        // calc each lang iteration
+        for (var i in det.children) {
+            for (var j in det.children[i].children) {
+                if (det.children[i].children[j].data && det.children[i].children[j].data.language) {
+                    tmp.push(det.children[i].children[j].data.language);
+                }
+            }
+        }
+
+        // sum up duplicates
+        tmp = count(tmp);
+
+        // find best lang
+        var topLang = 0, lang, totalLang = 0;
+        for (var k in tmp) {
+            totalLang += tmp[k];
+            if (tmp[k] > topLang) {
+                topLang = tmp[k];
+                lang = k;
+            }
+        }
+
+        resolve({
+            langcode: lang,
+            probability: ((topLang / totalLang) * 100).toFixed(2),
+            detection: tmp
+        });
+    });
+};
+
+var buildResponse = function (inc) {
+    var iso639 = require('./iso-639.json');
+    var declang = iso639[inc.langcode];
+
+    if (!declang) throw new Error('Not a recognized language');
+
+    if (declang.bibliographic) {
+        declang.terminologic = iso639[declang.bibliographic].terminologic || null;
+    } else {
+        declang.bibliographic = declang.terminologic ? iso639[declang.terminologic].bibliographic : null;
+    }
+    if (declang.bibliographic == null && declang.terminologic == null) {
+        declang.iso6392 = inc.langcode;
+    } else {
+        declang.iso6392 = null;
+    }
+
+    declang.probability = inc.probability;
+    declang.detected_langs = inc.detection;
+
+    return declang;
+}
+
+var DetectLang = module.exports = function (input) {
+    return new Promise(function (resolve, reject) {
+        parseInput(input)
+            .then(getLang)
+            .then(parseLang)
+            .then(buildResponse)
+            .then(resolve)
+            .catch(reject);
+    });
+};
